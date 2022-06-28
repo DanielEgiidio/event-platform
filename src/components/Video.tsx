13fe810ad76b8e2d986476c5deb040517395dcf9
DanielEgidio <danielegidio@si.fiponline.edu.br>
2022-06-28 18:16:16 -0300

    Correção do posicionamento do Video

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -33,7 +33,7 @@ export function Video(props: VideoProps) {
 
   return (
     <div className="flex-1">
-      <div className="bg-black">
+      <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
           <Player>
             <Youtube videoId={data.lesson.videoId} />
@@ -44,9 +44,10 @@ export function Video(props: VideoProps) {
 
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
-          <div className="flex-1 ">
+          <div className="flex-1">
             <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
-            <p className="mt-4 text-gray-200 leading-relaxed">
+
+            <p className="mt-6 text-gray-200 leading-relaxed">
               {data.lesson.description}
             </p>
 
@@ -71,19 +72,18 @@ export function Video(props: VideoProps) {
 
           <div className="flex flex-col gap-4">
             <a
-              href="#"
-              className="p-4 text-sm bg-green-500 flex items center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors"
+              href=""
+              className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors"
             >
               <DiscordLogo size={24} />
-              Comunidade do Discord
+              Comunidade do discord
             </a>
-
             <a
-              href="#"
-              className="p-4 text-sm border border-blue-500 text-blue-500 flex items center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors"
+              href=""
+              className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors"
             >
               <Lightning size={24} />
-              Acesse o Desafio
+              Acesse o desafio
             </a>
           </div>
         </div>
@@ -91,13 +91,13 @@ export function Video(props: VideoProps) {
         <div className="gap-8 mt-20 grid grid-cols-2">
           <a
             href=""
-            className="bg-gray-700 rounded overflow-hidden flex flex-items-stretch gap-6 hover:bg-gray-600 transition-colors"
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
           >
             <div className="bg-green-700 h-full p-6 flex items-center">
               <FileArrowDown size={40} />
             </div>
             <div className="py-6 leading-relaxed">
-              <strong className="text-2xl">Material Complementar</strong>
+              <strong className="text-2xl">Material complementar</strong>
               <p className="text-sm text-gray-200 mt-2">
                 Acesse o material complementar para acelerar o seu
                 desenvolvimento
@@ -107,9 +107,10 @@ export function Video(props: VideoProps) {
               <CaretRight size={24} />
             </div>
           </a>
+
           <a
             href=""
-            className="bg-gray-700 rounded overflow-hidden flex flex-items-stretch gap-6 hover:bg-gray-600 transition-colors"
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
           >
             <div className="bg-green-700 h-full p-6 flex items-center">
               <FileArrowDown size={40} />
